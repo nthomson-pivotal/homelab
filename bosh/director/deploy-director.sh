5ba65903c5576273e5ba0805c43c20e2b2337785
Niall Thomson <nthomson@pivotal.io>
2019-03-27 14:19:59 -0700

    Added director opsfile to create uaa credentials for concourse to access credhub

--- bosh/director/deploy-director.sh
+++ bosh/director/deploy-director.sh
@@ -5,11 +5,12 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 bosh -n create-env ~/projects/bosh-deployment/bosh.yml \
-    --state=$DIR/state.json \
-    --vars-store=$DIR/creds.yml \
+    --state=$DIR/state/state.json \
+    --vars-store=$DIR/state/creds.yml \
     -o ~/projects/bosh-deployment/vsphere/cpi.yml \
     -o ~/projects/bosh-deployment/jumpbox-user.yml \
     -o ~/projects/bosh-deployment/misc/dns.yml \
     -o ~/projects/bosh-deployment/uaa.yml \
     -o ~/projects/bosh-deployment/credhub.yml \
+    -o $DIR/operations/concourse-uaa.yml \
     -l $DIR/vars.yml
