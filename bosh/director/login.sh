b2d599ad66087b35046e49c22306583a1f30ce16
Niall Thomson <nthomson@pivotal.io>
2019-03-28 00:12:44 -0700

    Fixed director login script to use state directory path

--- bosh/director/login.sh
+++ bosh/director/login.sh
@@ -2,6 +2,8 @@
 
 #SOURCE ME
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 export BOSH_ENVIRONMENT=lab
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=`bosh int ./creds.yml --path /admin_password`
+export BOSH_CLIENT_SECRET=`bosh int $DIR/state/creds.yml --path /admin_password`
