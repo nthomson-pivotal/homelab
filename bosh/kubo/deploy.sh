36a9dba09ec805cef4b8c870b7652ff8880193f0
Niall Thomson <nthomson@pivotal.io>
2019-03-29 11:31:38 -0700

    Kubo: master is no longer static IP, adjusted api host name and rebalanced nodes AZ-wise

--- bosh/kubo/deploy.sh
+++ bosh/kubo/deploy.sh
@@ -7,8 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 kubo_deployment=$DIR/kubo-deployment-0.30.0
 
 bosh -n deploy $kubo_deployment/manifests/cfcr.yml \
-  -o $kubo_deployment/manifests/ops-files/misc/scale-to-one-az.yml \
   -o $kubo_deployment/manifests/ops-files/add-hostname-to-master-certificate.yml \
-  -o $kubo_deployment/manifests/ops-files/iaas/vsphere/master-static-ip.yml \
   -o $kubo_deployment/manifests/ops-files/vm-types.yml \
+  -o $DIR/operations/set-scale.yml \
   -l $DIR/vars.yml
