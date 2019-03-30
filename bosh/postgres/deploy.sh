aefbc6ea47af7f8e592ac046d6c97daa3e988d1d
Niall Thomson <nthomson@pivotal.io>
2019-03-30 11:21:35 -0700

    Added vault database/role

--- bosh/postgres/deploy.sh
+++ bosh/postgres/deploy.sh
@@ -6,5 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export BOSH_DEPLOYMENT=postgres
 
-bosh -n deploy $DIR/manifest.yml #-l vars.yml
+bosh -n deploy $DIR/manifest.yml
 
