f77fb66074fd569164c712d57342f0ff80d50aa4
Niall Thomson <nthomson@pivotal.io>
2019-03-30 08:43:12 -0700

    Added both, postgres and concourse monitoring

--- bosh/prometheus/deploy.sh
+++ bosh/prometheus/deploy.sh
@@ -4,9 +4,15 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+prometheus_deployment=$DIR/prometheus-boshrelease-24.1.0
+
 export BOSH_DEPLOYMENT=prometheus
 
-bosh -n deploy $DIR/prometheus-boshrelease-24.1.0/manifests/prometheus.yml \
+bosh -n deploy $prometheus_deployment/manifests/prometheus.yml \
+  -o $prometheus_deployment/manifests/operators/monitor-bosh.yml \
+  -o $prometheus_deployment/manifests/operators/enable-bosh-uaa.yml \
+  -o $prometheus_deployment/manifests/operators/monitor-postgres.yml \
+  -o $prometheus_deployment/manifests/operators/monitor-concourse.yml \
   -o $DIR/operations/external-postgres.yml \
   -o $DIR/operations/no-nginx.yml \
-  -l vars.yml
+  -l $DIR/vars.yml
