c4d9b2bc38c60ac6f0dfa02a6fefb1b90f9bda16
Niall Thomson <nthomson@pivotal.io>
2019-03-30 11:21:12 -0700

    Added global deploy script

---
+++ bosh/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+$DIR/caddy/deploy.sh
+$DIR/postgres/deploy.sh
+$DIR/minio/deploy.sh
+$DIR/prometheus/deploy.sh
+$DIR/vault/deploy.sh
+$DIR/kubo/deploy.sh
