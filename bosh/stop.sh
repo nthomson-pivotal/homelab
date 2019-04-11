ab3339b474f4524a369f9b3af9969c2a59d40b53
Niall Thomson <nthomson@pivotal.io>
2019-04-11 08:28:32 -0700

    Added stop script for bosh director deployments

---
+++ bosh/stop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source $DIR/director/login.sh
+for deployment in $(bosh deployments --json | jq -r '.Tables[].Rows[].name'); do
+  bosh -n -d $deployment stop --hard
+done
