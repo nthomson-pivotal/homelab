b8d44a68d1d095b89616f8bcc419386e491eb362
Niall Thomson <nthomson@pivotal.io>
2019-08-09 21:58:34 -0700

    Fix login to use director IP

--- bosh/director/login.sh
+++ bosh/director/login.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $DIR/common.sh
 
-export BOSH_ENVIRONMENT=lab
+export BOSH_ENVIRONMENT=172.16.6.6
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=`bosh int $STATE_DIR/creds.yml --path /admin_password`
