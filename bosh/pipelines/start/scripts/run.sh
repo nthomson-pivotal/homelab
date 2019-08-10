4e21a64d22e4ef1dcbfd528ddac6d9f3b1b93b64
Niall Thomson <nthomson@pivotal.io>
2019-08-09 22:07:24 -0700

    Messing

--- bosh/pipelines/start/scripts/run.sh
+++ bosh/pipelines/start/scripts/run.sh
@@ -8,6 +8,8 @@ tar zxf director-state/state-0.tgz
 
 export STATE_DIR=$(pwd)
 
+export BOSH_CA_CERT=$(bosh int $STATE_DIR/creds.yml --path /director_ssl/ca)
+
 source $DIR/../../../director/login.sh
 
 env
