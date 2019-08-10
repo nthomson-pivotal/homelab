3ebb9ac229c8e8a167b4471ae1ef16db4f683201
Niall Thomson <nthomson@pivotal.io>
2019-08-09 18:34:48 -0700

    Skip drain for cfcr on stop to avoid kubelet drain problems

--- bosh/stop.sh
+++ bosh/stop.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/director/login.sh
 
 bosh -n -d prometheus stop --hard
-bosh -n -d cfcr stop --hard
+bosh -n -d cfcr stop --hard --skip-drain
 bosh -n -d vault stop --hard
 bosh -n -d concourse stop --hard
 bosh -n -d minio stop --hard
